cb1018198276e128046fc221d10a1131f31079a0
Hugueds <hugo-pdf@hotmail.com>
2019-11-24 18:20:38 -0300

    Updates

---
+++ precache-manifest.20e6e2442ec50048a5e821f71f58e06a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3be68ce0ab7770c8812c86b8d503df",
+    "url": "/linha12/index.html"
+  },
+  {
+    "revision": "10d0d488cfad63810a6d",
+    "url": "/linha12/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7ad08eeac8e034b553d2",
+    "url": "/linha12/static/js/2.8dcb8a03.chunk.js"
+  },
+  {
+    "revision": "10d0d488cfad63810a6d",
+    "url": "/linha12/static/js/main.c2143f09.chunk.js"
+  },
+  {
+    "revision": "8a65817f799bcbce9cc8",
+    "url": "/linha12/static/js/runtime-main.07fdf94f.js"
+  }
+]);
